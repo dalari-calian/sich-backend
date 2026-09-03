93527a8e77c368e4d54c8ae9c38d7e4d31421e7a
dalari.calian <cdalari@gmail.com>
2026-09-03 19:45:24 -0300

    feat: base project

---
+++ src/main/java/com/sich/SichBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SichBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SichBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sich/SichBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SichBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
